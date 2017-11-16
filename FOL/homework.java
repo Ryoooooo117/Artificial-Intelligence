27ba6942111ca1cdbae9e47ca9a5c4902441accb
Ryoooooo117 <leon.he1228@gmail.com>
2017-11-16 09:16:31 -0800

    Update homework.java

--- FOL/homework.java
+++ FOL/homework.java
@@ -8,9 +8,21 @@ public class homework {
 	
 	public static void main(String[] args) throws IOException {
 		String s = "~F(x,Alice,y)";
-		String fileName = "input.txt";
+		String fileName = "HW3tc/9.txt";
 		Utility util = new Utility();
-		util.readFile(fileName);
-		System.out.println("res");
+		Resolution reso = new Resolution();
+		FileInfo file = util.readFile(fileName);
+		//util.showFile(file);
+		List<Boolean> outPut = new LinkedList<Boolean>();
+		for( Atomic q : file.query ){
+			if( reso.resolution(q, file.kb) ){
+				outPut.add(true);
+			}
+			else{
+				outPut.add(false);
+			}
+		}
+		for( boolean b : outPut ) System.out.println(b);
+		util.writeFile(outPut);
 	}
 }
